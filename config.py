aec3542d3390f24ced90104d8c8128ae1f8161c1
rjimenezh9 <82633854+rjimenezh9@users.noreply.github.com>
2026-06-18 08:29:35 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "TU_TOKEN_AQUI")
-TRIAL_DAYS = 30
 DONATION_LINK = os.environ.get("DONATION_LINK", "https://ko-fi.com/TU_USUARIO")
+DONATE_REMINDER_DAY = 10
